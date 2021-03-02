699f562d87d9243e59d599e2bc1dad24bce4ef51
web2cap <web2cap@gmail.com>
2021-03-02 22:59:59 +0300

    add form

--- bboard/admin.py
+++ bboard/admin.py
@@ -5,9 +5,17 @@ from django.contrib import admin
 from .models import Bb
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')
+    list_display = ('title', 'content', 'price', 'published','rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content', )
 
 #регистрация можеди в админке
 admin.site.register(Bb, BbAdmin)
+
+from .models import Rubric
+
+class RubricAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+
+admin.site.register(Rubric, RubricAdmin)
--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,3 @@
-#from django.shortcuts import render
-
-# Create your views here.
 """
 from django.http import HttpResponse
 from django.template import loader
@@ -14,11 +11,44 @@ def index(request):
     #return HttpResponse(s, content_type='text/plain; charset=utf-8')
     return HttpResponse(template.render(context,request))
 """
+
 from django.shortcuts import render
 from .models import Bb
 
+### INDEX ###
 def index(request):
     # сортировка перенесена в moderls -> Meta class
     #bbs = Bb.objects.order_by('-published')
-    bbs = Db.objects.all()
-    return render(request, 'bboard/index.html',{'bbs': bbs})
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+### RUBRIC ###
+from .models import Rubric
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context= {'bbs': bbs, 'rubrics': rubrics,
+              'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
+### ADD FORM ###
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import BbForm
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
